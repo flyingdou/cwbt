43c571997de8cdf4d6103110eaac035b4c186e1d
flyingdou <1576883859@qq.com>
2018-11-05 14:10:04 +0800

    tab Bar Icon修改

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
   },
   constant: {
     base_img_url:"https://fish.ecartoon.com.cn/picture",
-    base_req_url:"http://192.168.0.110:8080/hqwl/api/",
+    base_req_url:"http://192.168.0.111:8080/hqwl/api/",
     upload_url:'https://fish.ecartoon.com.cn/uploadFile'
   }
 })
