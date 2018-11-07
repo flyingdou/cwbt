80405962dacaf889315ae58a63128c6adedc95ba
flyingdou <1576883859@qq.com>
2018-11-07 10:59:34 +0800

    update host

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
   },
   constant: {
     base_img_url:"https://fish.ecartoon.com.cn/picture",
-    base_req_url:"http://192.168.0.105:8080/hqwl/api/",
+    base_req_url:"http://127.0.0.1:8080/hqwl/api/",
     upload_url:'https://fish.ecartoon.com.cn/uploadFile'
   },
   user: {}
