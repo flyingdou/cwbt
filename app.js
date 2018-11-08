0696681bc8e9fb74c0636ede9e9a564093792268
flyingdou <1576883859@qq.com>
2018-11-08 13:46:15 +0800

    merge head and master

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ App({
   constant: {
     base_img_url:"https://fish.ecartoon.com.cn/picture",
     base_req_url:"http://127.0.0.1:8080/hqwl/api/cwbtMP/",
-    upload_url:'https://fish.ecartoon.com.cn/uploadFile'
+    upload_url:'https://fish.ecartoon.com.cn/uploadFile',
+    logo: 'https://fish.ecartoon.com.cn/picture/201810274602.jpg'
   },
   user: {}
 })
