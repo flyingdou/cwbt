1b39ed3d890787554cd33969b4918d7c24500f92
flyingdou <1576883859@qq.com>
2018-11-16 17:30:13 +0800

    update123

---
+++ pages/switch/switch.js
@@ -0,0 +1,82 @@
+var app = getApp();
+var obj = null;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+     obj = this;
+     // 初始化多列选择器
+     // this.multiSelectorInit();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+
+  
+  // 跳转页面
+  goto: (e) => {
+    var link = e.currentTarget.dataset.link;
+    wx.navigateTo({
+      url: link
+    });
+    return;    
+  }
+
+  
+})
---
+++ pages/tempList/tempList.js
@@ -0,0 +1,167 @@
+var app = getApp();
+var obj = null;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+     obj = this;
+     obj.setData({
+       user_id: app.user.id
+     });
+     
+     // 部门id
+     var dept_id = options.dept_id;
+     if (dept_id) {
+       obj.data.dept_id = dept_id;
+     }
+
+     // 船舶id
+     var boat_id = options.boat_id; 
+     if (boat_id) {
+       obj.data.boat_id = boat_id;
+     }
+    
+
+    // 修改的列表
+    var isUpdate = options.isUpdate;
+    if (isUpdate) {
+        obj.setData({
+          isUpdate: isUpdate
+        });
+    }
+
+    obj.init();
+
+
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 初始化页面数据
+   */
+  init: () => {
+    var isUpdate = obj.data.isUpdate;
+    var reqUrl = app.constant.base_req_url + 'getWorkCardList.we';
+    var param = {};
+    param = {
+      type: '2',
+      dept_id: app.user.deptId,
+      status: '1,9' // 未完成、进行中
+    }
+
+    if (isUpdate) {
+      reqUrl = app.constant.base_req_url + 'getWorkcardUpdateAbleList.we';
+      param = {
+        executor_id: app.user.id
+      };
+    } 
+    
+    // console.log(param);
+    // return;
+    wx.request({
+      url: reqUrl,
+      dataType:'json',
+      data:{
+        json: encodeURI(JSON.stringify(param))
+      },
+      success: (res) => {
+        res = res.data;
+        if (res.success) {
+          obj.setData({
+            taskList: res.workCardList
+          });
+        }
+      }
+    })
+
+  },
+
+  /**
+   * 跳转到任务详情页面
+   */
+  goto: (e) => {
+    var index = e.currentTarget.dataset.index;
+    var workCard = obj.data.taskList[index];
+    var isUpdate = obj.data.isUpdate;
+    if (workCard.status == 9 && workCard.collectorpersonid != app.user.id) {
+      wx.showModal({
+        title: '提示',
+        content: '该工作已在进行中，请选择其他工作！',
+        showCancel: false
+      })
+      return;
+    }
+
+    // 跳转传参
+    var redUrl = '../../pages/workCardDetail/workCardDetail?workCardId=' + workCard.id;
+    if (isUpdate) {
+      redUrl = redUrl + '&isRollback=true';
+    }
+
+    wx.redirectTo({
+      url: redUrl,
+    })
+  },
+
+
+})
