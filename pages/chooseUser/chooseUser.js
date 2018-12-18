62114426fa3e3bfd95624a987af07ea4f9fd295c
flyingdou <1576883859@qq.com>
2018-12-18 17:09:10 +0800

    发起督导选择用户

--- pages/chooseUser/chooseUser.js
+++ pages/chooseUser/chooseUser.js
@@ -76,20 +76,7 @@ Page({
    * 初始化页面数据
    */
   init: () => {
-    var navList = [
-      {
-        "name":"荆州航道处",
-        "deptid":21
-      },
-      {
-        "name": "荆州测绘中心",
-        "deptid": 216
-      }
-    ];
-    obj.setData({
-      navList: navList
-    });
-
+    var navList = [];
     var reqUrl = util.getRequestURL('getDepartmentUser.we');
     var param = {};
     param.dept_id = app.user.deptId;
@@ -108,9 +95,11 @@ Page({
       success:(res) => {
         res = res.data;
         if (res.success) {
+          navList.push(res.dept);
           obj.setData({
             deptList: res.deptList,
-            userList: res.userList
+            userList: res.userList,
+            navList: navList
           });
         }
       },
@@ -120,5 +109,170 @@ Page({
     })
 
   },
+
+
+
+  /**
+   * next,查询下级部门中的人员
+   */
+  next: (e) => {
+    // 清空storage中的值
+    wx.removeStorageSync('chooseUsers');
+     var navList = obj.data.navList;
+     var nav = e.currentTarget.dataset.dept;
+     navList.push(nav);
+     obj.setData({
+       navList: navList
+     });
+     obj.getNext();
+  },
+
+  /**
+   * 向上选择部门
+   */
+  up: (e) => {
+    // 清空storage中的值
+    wx.removeStorageSync('chooseUsers');
+    var index = e.currentTarget.dataset.index;
+    var navList = obj.data.navList;
+
+    // 选中当前部门，不做操作
+    if (index == (navList.length - 1)) {
+      return;
+    }
+
+    // 非当前部门，截取选中处之前的
+    var doux = navList.slice(0, index + 1);
+    obj.setData({
+      navList: doux
+    });
+    
+    obj.getNext();
+  },
+
+
+  /**
+   * 查询下级数据
+   */
+  getNext: () => {
+    var navList = obj.data.navList;
+    var reqUrl = util.getRequestURL('getDepartmentUser.we');
+    var param = {};
+    param.dept_id = navList[navList.length-1].seq_id;
+    param.type = 'nextDept';
+    wx.showLoading({
+      title: '加载中...',
+    })
+    wx.request({
+      url: reqUrl,
+      data: {
+        json: encodeURI(JSON.stringify(param))
+      },
+      dataType: 'json',
+      success: (res) => {
+        res = res.data;
+        if (res.success) {
+           obj.setData({
+             deptList: res.deptList,
+             userList: res.userList
+           });
+        }
+      },
+      complete: (rx) => {
+        wx.hideLoading();
+      }
+    })
+  },
+
+  /**
+   * checkboxChange
+   */
+  checkboxChange: (e) => {
+    // 选中的下标数组
+     var index = e.currentTarget.dataset.index;
+     var indexs = obj.data.indexs || [];
+     var userList = obj.data.userList;
+     
+      if (userList[index].checked) {
+        userList[index].checked = false;
+        for (var i in indexs) {
+          if (indexs[i] == index) {
+            indexs.splice(i, 1);
+          }
+        }
+      } else {
+        userList[index].checked = true;
+        indexs.push(index);
+      }
+      
+      // 存储数据
+      obj.setData({
+        indexs: indexs,
+        userList: userList
+      });
+
+  },
+
+
+  /**
+   * chooseAll
+   */
+  chooseAll: () => {
+    var chooseAll = obj.data.chooseAll || false;
+    var indexs = obj.data.indexs || [];
+    var userList = obj.data.userList || [];
+    if (chooseAll) {
+      chooseAll = false;
+      for (var u in userList) {
+        userList[u].checked = false;
+      }
+      indexs = [];
+    } else {
+      chooseAll = true;
+      for (var u in userList) {
+        userList[u].checked = true;
+        indexs.push(u);
+      }
+    }
+
+    obj.setData({
+      chooseAll: chooseAll,
+      indexs: indexs,
+      userList: userList
+    });
+
+
+  },
+
+
+  choose: () => {
+    var userList = obj.data.userList || [];
+    var chooseUsers = [];
+    for (var u in userList) {
+      if (userList[u].checked) {
+        chooseUsers.push(userList[u]);
+      }
+    }
+    
+    if (chooseUsers.length < 1) {
+      wx.showModal({
+        title: '提示',
+        content: '请选择人员',
+        showCancel: false,
+      })
+      return;
+    }
+
+    // 将值存储起来
+    wx.setStorageSync('chooseUsers', chooseUsers);
+
+    wx.navigateBack({
+      delta: 1,
+    })
+
+
+  }
+
+  
 
 })
