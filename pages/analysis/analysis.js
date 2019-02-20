8ae04aa02c66de1fb927fc5da59d05931ded6595
w1441639547 <1441639547@qq.com>
2019-02-20 15:31:34 +0800

    数据分析饼图优化

--- pages/analysis/analysis.js
+++ pages/analysis/analysis.js
@@ -1,6 +1,6 @@
-import * as echarts from '../../ec-canvas/echarts';
 var app = getApp();
 var util = require('../../utils/util.js');
+var wxcharts = require('../../utils/wxcharts-min.js');
 var obj = null;
 Page({
 
@@ -8,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    ec: {}
+    
   },
 
   /**
@@ -16,6 +16,9 @@ Page({
    */
   onLoad: function (options) {
     obj = this;
+
+    // 页面初始化
+    obj.init();
   },
 
   /**
@@ -86,12 +89,19 @@ Page({
           sum += item.number5;
         });
         var data = [];
-        data.push({value: Math.round((notComplete/sum) * 100), name: "未完成"});
-        data.push({value: Math.round((complete/sum) * 100), name: "正常完成"});
-        data.push({value: Math.round((advance/sum) * 100), name: "提前完成"});
-        data.push({value: Math.round((overdue/sum) * 100), name: "逾期完成"});
-        obj.data.optionData = data;
-        obj.echartInit(e);
+        data.push({data: Math.round((notComplete/sum) * 100), name: "未完成"});
+        data.push({data: Math.round((complete/sum) * 100), name: "正常完成"});
+        data.push({data: Math.round((advance/sum) * 100), name: "提前完成"});
+        data.push({data: Math.round((overdue/sum) * 100), name: "逾期完成"});
+        obj.wxcharts = new wxcharts({
+          canvasId: "pieCanvas",
+          width: 300,
+          height: 300,
+          type: "pie",
+          animation: true,
+          series: data,
+          dataLabel: true
+        });
         wx.hideLoading();
       },
       fail(e) {
@@ -100,55 +110,5 @@ Page({
         console.log(e);
       }
     });
-  },
-
-  /**
-   * echart初始化
-   */
-  echartInit (e) {
-    obj.initChart(e.detail.canvas, e.detail.width, e.detail.height);
-  },
-
-  /**
-   * 饼图初始化
-   */
-  initChart(canvas, width, height) {
-    const chart = echarts.init(canvas, null, {
-      width: width,
-      height: height
-    });
-    canvas.setChart(chart);
-    chart.setOption(obj.getOption());
-    return chart;
-  },
-
-  /**
-   * 获取饼图数据
-   */
-  getOption () {
-    var option = {
-      backgroundColor: "#ffffff",
-      color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
-      animation: true,
-      series: [{
-        label: {
-          normal: {
-            fontSize: 14
-          }
-        },
-        type: 'pie',
-        center: ['50%', '50%'],
-        radius: [0, '60%'],
-        data: obj.data.optionData,
-        itemStyle: {
-          emphasis: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 2, 2, 0.3)'
-          }
-        }
-      }]
-    };
-    return option;
   }
 })
